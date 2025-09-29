1d115c661c5efadb2cb3dc5c3efd7a5e2c53ff53
kuroweb <kuromitsu0104@gmail.com>
2025-09-29 21:54:42 +0900

    feat: json loggerを追加

--- apps/web/workers/app.ts
+++ apps/web/workers/app.ts
@@ -2,6 +2,8 @@ import { Hono } from "hono";
 import { basicAuth } from "hono/basic-auth";
 import { contextStorage } from "hono/context-storage";
 import { createRequestHandler } from "react-router";
+import { requestId } from "hono/request-id";
+import type { RequestIdVariables } from "hono/request-id";
 
 declare module "react-router" {
   export interface AppLoadContext {
@@ -12,16 +14,52 @@ declare module "react-router" {
   }
 }
 
-const app = new Hono<{ Bindings: Env }>();
+const app = new Hono<{ Bindings: Env; Variables: RequestIdVariables }>();
+
 app.use(contextStorage());
 
-// Basic認証
+// JSON Logger
+app.use("*", requestId());
+app.use(async (c, next) => {
+  const start = Date.now();
+  const base = {
+    time: new Date(start).toISOString(),
+    requestId: c.get("requestId"),
+    phase: "start",
+    method: c.req.method,
+    path: c.req.path,
+  };
+
+  console.log(
+    JSON.stringify({
+      ...base,
+      status: null,
+      elapsed: null,
+    })
+  );
+
+  await next();
+
+  const end = Date.now();
+  console.log(
+    JSON.stringify({
+      ...base,
+      time: new Date(end).toISOString(),
+      phase: "finished",
+      status: c.res.status,
+      elapsed: `${end - start}ms`,
+    })
+  );
+});
+
+// Basic Auth
 app.use(async (c, next) => {
   const username = c.env.BASIC_AUTH_USER;
   const password = c.env.BASIC_AUTH_PASS;
   return basicAuth({ username, password })(c, next);
 });
 
+// React Router
 app.use(async (c) => {
   // @ts-ignore
   const requestHandler = createRequestHandler(
