a0119189b813bf470ee667ae6300e686565b55d0
kuroweb <kuromitsu0104@gmail.com>
2025-09-30 23:52:42 +0900

    feat: drizzle周りをセットアップ

--- apps/web/drizzle.config.ts
+++ apps/web/drizzle.config.ts
@@ -1,21 +1,47 @@
 import type { Config } from "drizzle-kit";
 import dotenv from "dotenv";
+import { readdirSync } from "fs";
+import { join } from "path";
 
 const currentEnv = process.env.ENV ?? "development";
-dotenv.config({
-  path: currentEnv === "production" ? ".prod.vars" : ".dev.vars",
-});
+const isProduction = currentEnv === "production";
 
-export default {
-  schema: "./app/db/schema.ts",
-  out: "./migrations",
-  dialect: "sqlite",
-  driver: "d1-http",
-  dbCredentials: {
-    accountId: process.env.CLOUDFLARE_ACCOUNT_ID ?? "",
-    databaseId: process.env.CLOUDFLARE_D1_DB_ID ?? "",
-    token: process.env.CLOUDFLARE_API_TOKEN ?? "",
-  },
-  strict: true,
-  verbose: true,
-} satisfies Config;
+// 本番実行時は.prod.varsを読み込む
+if (isProduction) dotenv.config({ path: ".prod.vars" });
+
+// ローカル環境のD1のSQLiteファイルのパスを取得
+function getSqlitePath(): string {
+  const wranglerStatePath =
+    "./.wrangler/state/v3/d1/miniflare-D1DatabaseObject";
+  const files = readdirSync(wranglerStatePath);
+  const sqliteFile = files.find((file) => file.endsWith(".sqlite"));
+
+  if (!sqliteFile) throw new Error("Wrangler SQLite Not Found.");
+
+  return join(wranglerStatePath, sqliteFile);
+}
+
+export default (isProduction
+  ? {
+      schema: "./app/db/schema.ts",
+      out: "./migrations",
+      dialect: "sqlite",
+      driver: "d1-http",
+      dbCredentials: {
+        accountId: process.env.CLOUDFLARE_ACCOUNT_ID ?? "",
+        databaseId: process.env.CLOUDFLARE_D1_DB_ID ?? "",
+        token: process.env.CLOUDFLARE_API_TOKEN ?? "",
+      },
+      strict: true,
+      verbose: true,
+    }
+  : {
+      schema: "./app/db/schema.ts",
+      out: "./migrations",
+      dialect: "sqlite",
+      dbCredentials: {
+        url: getSqlitePath(),
+      },
+      strict: true,
+      verbose: true,
+    }) satisfies Config;
--- apps/web/migrations/0001_create_users.sql
+++ apps/web/migrations/0001_create_users.sql
@@ -1,12 +1,8 @@
 -- Migration number: 0001 	 2025-09-27T23:37:18.568Z
 
--- users テーブル作成
 CREATE TABLE IF NOT EXISTS users (
   id TEXT PRIMARY KEY,
   password TEXT NOT NULL,
   created_at TEXT NOT NULL DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ','now')),
   updated_at TEXT NOT NULL DEFAULT (strftime('%Y-%m-%dT%H:%M:%fZ','now'))
 );
-
--- デモ用の初期ユーザー（平文パスワード: admin）
-INSERT OR IGNORE INTO users (id, password) VALUES ('admin', 'admin');
