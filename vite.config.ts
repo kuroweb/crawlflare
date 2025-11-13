f711f1fccc85783ce447abb46c536e3f4246d6a1
kuroweb <kuromitsu0104@gmail.com>
2025-11-14 01:16:47 +0900

    feat: update HMR settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,10 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import serverAdapter, { defaultOptions } from "@hono/vite-dev-server";
-import type { cloudflareAdapter } from "@hono/vite-dev-server/cloudflare";
+import { cloudflare } from "@cloudflare/vite-plugin";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { getPlatformProxy, unstable_getVarsForDev } from "wrangler";
 
-// Entry file
-const entry = "./backend/app.ts";
-
-// Prevent tampering with Hono's Cloudflare parameters executed by default
-const adapter: typeof cloudflareAdapter = async (options) => {
-  const proxy = await getPlatformProxy(options?.proxy);
-  return {
-    env: proxy.env,
-    executionContext: proxy.ctx,
-    onServerClose: () => proxy.dispose(),
-  };
-};
-
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   resolve: {
     alias: [
       {
@@ -28,31 +13,8 @@ export default defineConfig({
       },
     ],
   },
-  ssr: {
-    resolve: {
-      externalConditions: ["worker"],
-    },
-  },
   plugins: [
-    serverAdapter({
-      adapter,
-      entry,
-      exclude: [
-        ...defaultOptions.exclude,
-        /(\.webp|png|svg)(\?.*)?$/,
-        /\?import(\&.*)?$/, // development adjustment
-      ],
-      // HMR adjustment
-      handleHotUpdate: ({ server, modules }) => {
-        const isServer = modules.some((mod) => {
-          return mod._ssrModule?.id && !mod._clientModule;
-        });
-        if (isServer) {
-          server.hot.send({ type: "full-reload" });
-          return [];
-        }
-      },
-    }),
+    mode !== "production" && cloudflare({ viteEnvironment: { name: "ssr" } }),
     tailwindcss(),
     reactRouter(),
     tsconfigPaths(),
@@ -60,4 +22,4 @@ export default defineConfig({
   experimental: { enableNativePlugin: true },
   // NOTE: daisyuiをapp.cssでロードするとnpm run buildでlightningcssのwarningが出るので、回避策としてesbuildでminifyしている
   build: { cssMinify: "esbuild" },
-});
+}));
